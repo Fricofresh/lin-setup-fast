fff17f4b950c90c5dfb8088991eaeafaaa25cefe
Fricofresh Grayman <k.boehmer@outlook.de>
2025-10-12 17:58:10 +0200

    fixed locale error

--- config/setupGlobalLocale.sh
+++ config/setupGlobalLocale.sh
@@ -1,6 +1,7 @@
 grep -q "LC_TIME=de_DE.UTF-8" /etc/locale.conf
 if [ $? -ne 0 ]; then
   echo LC_TIME=de_DE.UTF-8 >> /etc/locale.conf
-elif [ "$?" -eq 1 ]; then
+fi
+if [ "$?" -eq 1 ]; then
   sed -i 's/LC_TIME=.*/LC_TIME=de_DE.UTF-8/' /etc/locale.conf
 fi
